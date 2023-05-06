25765daabab626f400468c816814d2719966294b
Liedsonfsa <111002008+Liedsonfsa@users.noreply.github.com>
2023-05-06 15:02:05 -0300

    Update

--- Lista-2/I-Can-Guess-the-Data-Structure.cpp
+++ Lista-2/I-Can-Guess-the-Data-Structure.cpp
@@ -6,22 +6,54 @@ int main() {
 
     int n;
 
-    while( cin >> n ){
-        int op, num;
-        vector <int> input, output;
+    while(cin >> n){
+        int maior, ultimo, primeiro;
+        int q = 0, s = 0, i = 0, pq = 0;
+        vector <int> push, pop;
+        stack <int> pilha;
+        int l = 0;
+        while( n-- ){
+            int op, valor;
 
-        for(int i = 1; i <= n; i++){
-            cin >> op >> num;
+            cin >> op >> valor;
+
+            if(l == 0 && op == 1){
+                maior = valor;
+                primeiro = valor;
+            }
 
             if(op == 1){
-                input.push_back(num);
-            } else{
-                output.push_back(num);
+                ultimo = valor;
             }
+
+            if(valor > maior && op == 1){
+                maior = valor;
+            }
+
+            if(op == 1){
+                push.push_back(valor);
+            } else{
+                pop.push_back(valor);
+            } 
+        }
+
+
+        if(push[0] == pop[0]){
+            cout << "queue" << endl;
+        } else if(pop[0] == maior && (pop[0] == push[push.size() - 1])){
+            cout << "not sure" << endl;
+        } else if(pop[0] == push[push.size() - 1]){
+            cout << "stack" << endl;
+        } else if(pop[0] == maior){
+            cout << "priority queue" << endl;
+        } else{
+            cout << "impossible" << endl;
         }
 
         
     }
+
+    
     
     return 0;
 }
