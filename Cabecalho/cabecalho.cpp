29d1ee58d2662a435efdba0d01fa6064b7c86308
Liedsonfsa <111002008+Liedsonfsa@users.noreply.github.com>
2023-06-02 13:13:58 -0300

    Update

--- Cabecalho/cabecalho.cpp
+++ Cabecalho/cabecalho.cpp
@@ -4,13 +4,17 @@ using namespace std;
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define DIV(a, b) ((a) / (b))
+#define MULT(a, b) ((a) * (b))
+#define SOMA(a, b) ((a) + (b))
+#define SUB(a, b) ((a) - (b))
 #define endl '\n'
 #define pb push_back
 
 int main() {
 
 
-    
+
 
 
 
